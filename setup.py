d22356c40e88b3d08d52a1c7e56f21a8f0cd5ad6
ismaelpadilla <padillaismael92@gmail.com>
2020-02-16 21:22:48 -0300

    added setup.py, manifest

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='ci_uy',
+    version='0.1',
+    description='TODO',
+    url='https://github.com/ismaelpadilla/ci_uy_py',
+    author='Ismael Padilla',
+    author_email='padillaismael92@gmail.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['ci_uy'],
+    include_package_data=True,
+    zip_safe=False)
+
+'''
+TODO
+    long_description
+    keywords
+'''
