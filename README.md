6629722302d1e18ee7ac21ff06cf7669e9014b9e
ismaelpadilla <padillaismael92@gmail.com>
2020-02-18 20:28:05 -0300

    Added setup and modified readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ci-uy-py
+# ciuy
+
+Package for validating Uruguayan ids. Based on the ci_uy gem by Fernando Briano.
+
+Work in progress.
+
+# Installation
+
+```
+pip install ciuy
+```
+
+# Testing
+
+You can run tests from the project root after cloning with `nose2`.
--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 from setuptools import setup
 
-setup(name='ci_uy',
-    version='0.1',
-    description='TODO',
-    url='https://github.com/ismaelpadilla/ci_uy_py',
-    author='Ismael Padilla',
-    author_email='padillaismael92@gmail.com',
-    license='MIT',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    packages=['ci_uy'],
-    include_package_data=True,
-    zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='ciuy',
+      version='0.2',
+      description='Package for validating Uruguayan ids (work in progress).',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords=["Uruguay", "dni", "id", "eid", "validation"],
+      url='https://github.com/ismaelpadilla/ci_uy_py',
+      author='Ismael Padilla',
+      author_email='padillaismael92@gmail.com',
+      license='MIT',
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      packages=['ciuy'],
+      include_package_data=True,
+      zip_safe=True)
 
 '''
 TODO
     long_description
     keywords
+READ
+    https://packaging.python.org/guides/distributing-packages-using-setuptools/
 '''
